3a9a08c901eca3cca614c8b509d484e1fd4ed7e1
Manas Patil <patilmanas1304@gmail.com>
2026-05-04 20:32:01 +0530

    After embedding and saving the file in the ChromaDB and BM25 Indices. Save the file in a permanent directory and also save the document information on the database using the ProjectDocument model

--- backend/rag_pipeline/rag_ingestion_pipeline.py
+++ backend/rag_pipeline/rag_ingestion_pipeline.py
@@ -1,5 +1,8 @@
-import os, time
+import os, time, shutil
 from .rag_ingestion_functions import partition_document, create_chunks_by_title, process_chunks, create_vector_store
+
+from database import SessionLocal
+import models
 
 def rag_ingestion(file_path: str, project_id: int):
   start=time.time()
@@ -18,6 +21,26 @@ def rag_ingestion(file_path: str, project_id: int):
     print(f"[Project {project_id}] Step 4: Embedding and saving to ChromaDB Vault...")
     create_vector_store(langchain_documents, project_id)
 
+    print(f"[Project {project_id}] Step 5: Permanently saving document...")
+    permanent_dir = f"./media/projects/{project_id}/documents"
+    os.makedirs(permanent_dir, exist_ok=True)
+    filename = os.path.basename(file_path)
+    permanent_path = os.path.join(permanent_dir, filename)
+    shutil.move(file_path, permanent_path)
+
+    db=SessionLocal()
+    try:
+      new_document=models.ProjectDocument(
+        project_id=project_id,
+        file_name=filename,
+        file_path=permanent_path,
+      )
+      db.add(new_document)
+      db.commit()
+      print(f"[Project {project_id}] 💾 Document logged in database!")
+    finally:
+      db.close()
+
     end=time.time()
     print(f"[Project {project_id}] ✅ Ingestion Complete in {round(end - start, 2)} seconds!")
     return True
@@ -25,5 +48,5 @@ def rag_ingestion(file_path: str, project_id: int):
     raise e
   finally:
     if os.path.exists(file_path):
-      os.remove(file_path)
-      print(f"[Project {project_id}] 🧹 Cleaned up temporary file: {file_path}")
+        os.remove(file_path)
+        print(f"[Project {project_id}] 🧹 Ingestion failed. Cleaned up temp file.")
