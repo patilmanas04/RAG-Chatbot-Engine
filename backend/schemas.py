a31d5a1ac8fbfd2a7d3fe84026d8d0da9704a983
Manas Patil <patilmanas1304@gmail.com>
2026-05-07 00:51:38 +0530

    Add the UserResponse pydantic schema which will only include id, first_name, last_name and email

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,8 +1,19 @@
 from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
+  first_name: str
+  last_name: str
   email: EmailStr
   password: str
+
+class UserResponse(BaseModel):
+  id: int
+  first_name: str
+  last_name: str
+  email: str
+
+  class Config:
+    from_attributes=True
 
 class Token(BaseModel):
   access_token: str
